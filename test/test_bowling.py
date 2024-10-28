bfa9c6a549a407e25cb881f3d3a5f561e00b6127
Satu Kiiskinen <satsu.kiiskinen@gmail.com>
2024-10-28 16:57:47 +0200

    US1 - Test 2

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -13,3 +13,7 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(frame)
         self.assertEqual(frame, game.get_frame_at(0))
 
+    def test_empty_game(self):
+        game = BowlingGame()
+        self.assertRaises(BowlingError, game.get_frame_at, 0)
+
